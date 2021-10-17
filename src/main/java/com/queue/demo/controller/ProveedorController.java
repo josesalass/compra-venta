ec76d2731fd22b0e1b73fffbc2269d82c1adf07d
Catherine Neira <catherine.neira1801@alumnos.ubiobio.cl>
2021-10-16 21:22:25 -0300

    Creación de visualización y busqueda de entidad Proveedor

--- src/main/java/com/queue/demo/controller/ProveedorController.java
+++ src/main/java/com/queue/demo/controller/ProveedorController.java
@@ -5,6 +5,7 @@ import com.queue.demo.service.ProveedorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,4 +20,17 @@ public class ProveedorController {
 	public List<Proveedor> list(){
 		return proveedorService.buscarTodosLosProveedores();
 	}
+	
+	@GetMapping("/findNombre")
+	public List<Proveedor> buscarPorNombre (@RequestParam(value="nombre",required=true) String nombre){
+		return proveedorService.buscarPorNombre(nombre);
+	}
+	
+	@GetMapping("/findRut")
+	public List<Proveedor> buscarPorRut (@RequestParam(value="rut",required=true) String rut){
+		return proveedorService.buscarPorRut(rut);
+	}
 }
+	
+	
+		
--- src/main/java/com/queue/demo/service/ProveedorServiceImpl.java
+++ src/main/java/com/queue/demo/service/ProveedorServiceImpl.java
@@ -4,6 +4,11 @@ import com.queue.demo.model.Proveedor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 import com.queue.demo.repository.*;
@@ -11,14 +16,42 @@ import com.queue.demo.repository.*;
 @Service
 @Transactional
 public class ProveedorServiceImpl implements ProveedorService{
+	@PersistenceContext
+    private EntityManager em;
 	
 	@Autowired
 	RepositorioProveedor repProveedor;
 	
 	@Override
+	public List <Proveedor> buscarPorNombre (String nombre){
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Proveedor> criteriaQ = cb.createQuery(Proveedor.class);
+		Root<Proveedor> root = criteriaQ.from(Proveedor.class);
+		
+		criteriaQ.select(root).where(cb.equal(root.get("nombreempresa"), nombre));
+		
+		return em.createQuery(criteriaQ).getResultList();
+
+	}
+	
+	
+	@Override
+	public List <Proveedor> buscarPorRut (String rut){
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Proveedor> criteriaQ = cb.createQuery(Proveedor.class);
+		Root<Proveedor> root = criteriaQ.from(Proveedor.class);
+		
+		criteriaQ.select(root).where(cb.equal(root.get("rutempresa"), rut));
+		
+		return em.createQuery(criteriaQ).getResultList();
+
+	}
+	
+	@Override
 	public List<Proveedor> buscarTodosLosProveedores(){
 		return repProveedor.findAll();
 	}
+	/*
 	@Override
 	public Proveedor buscarProveedorPorRut(String rut) {
 		return repProveedor.findById(1).get();
@@ -28,6 +61,7 @@ public class ProveedorServiceImpl implements ProveedorService{
 	public Proveedor buscarProveedorPorNombre(String nombre) {
 		return repProveedor.findById(1).get();
 	}
+	*/
 	@Override
 	public void guardar(Proveedor proveedor) {
 		repProveedor.save(proveedor);
