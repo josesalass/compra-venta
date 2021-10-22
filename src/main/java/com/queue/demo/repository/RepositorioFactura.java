9be8dc22c8a0c56e7e6163f84d836859f50ac056
José Pedro Unda Montecinos <jose.unda1801@alumnos.ubiobio.cl>
2021-10-22 18:41:26 -0300

    borrando clases que no se ocuparán

--- src/main/java/com/queue/demo/repository/RepositorioFactura.java
+++
@@ -1,9 +0,0 @@
-package com.queue.demo.repository;
-
-import com.queue.demo.model.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RepositorioFactura extends JpaRepository <Factura, Integer> {
-
-}
--- src/main/java/com/queue/demo/service/BoletaService.java
+++
@@ -1,9 +0,0 @@
-package com.queue.demo.service;
-
-import java.util.List;
-
-import com.queue.demo.model.Boleta;
-
-public interface BoletaService {
-	public List<Boleta> buscarTodasLasBoletas();
-}
