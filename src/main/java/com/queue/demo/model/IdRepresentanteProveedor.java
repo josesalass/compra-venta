94a1c26977ac79f274e10c04559fd73fe48113be
Catherine Neira <catherine.neira1801@alumnos.ubiobio.cl>
2021-10-30 17:37:56 -0300

    Implementación Doble PrimaryKey Representante Proveedor

---
+++ src/main/java/com/queue/demo/model/IdRepresentanteProveedor.java
@@ -0,0 +1,37 @@
+package com.queue.demo.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class IdRepresentanteProveedor implements Serializable {
+	private String rutrep;
+	private String rutemp;
+	
+	
+	public IdRepresentanteProveedor(String rutrep, String rutemp) {
+		this.rutrep = rutrep;
+		this.rutemp = rutemp;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(rutemp, rutrep);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		IdRepresentanteProveedor other = (IdRepresentanteProveedor) obj;
+		return Objects.equals(rutemp, other.rutemp) && Objects.equals(rutrep, other.rutrep);
+	}
+	
+	
+}
