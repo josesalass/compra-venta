dcdd53a558dd8eba681bd74b163e15915bc3a954
josesalass <jose.salas1801@alumnos.ubiobio.cl>
2021-11-30 23:46:04 -0300

    Creación UsuarioServiceTest y Edición Clases y metodos asociadas a Usuario

--- src/main/java/com/queue/demo/service/UsuarioServiceImpl.java
+++ src/main/java/com/queue/demo/service/UsuarioServiceImpl.java
@@ -1,16 +1,15 @@
+
+
 package com.queue.demo.service;
+
 
 import com.queue.demo.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+
 import com.queue.demo.repository.RepositorioUsuario;
 
 
@@ -19,37 +18,35 @@ import com.queue.demo.repository.RepositorioUsuario;
 @Service
 @Transactional
 public class UsuarioServiceImpl implements UsuarioService{
-	@PersistenceContext
-    private EntityManager em;
-	
+
 	@Autowired
 	RepositorioUsuario repUsuario;
-	
-	@Override 
+
+	@Override
 	public List<Usuario> buscarTodosLosUsuarios(){
 		return repUsuario.findAll();
 	}
-	@Override 
-    public Usuario buscarUsuarioPorRut(String rut) {
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<Usuario> criteriaQ = cb.createQuery(Usuario.class);
-		Root<Usuario> root = criteriaQ.from(Usuario.class);
-		criteriaQ.select(root).where(cb.equal(root.get("rutusuario"),rut));
-		if(em.createQuery(criteriaQ).getResultList().isEmpty()) {
-			return null;
+
+
+	@Override
+	public Optional<Usuario> buscarUsuarioPorRut(String idusuario) {
+		Optional<Usuario> optional = repUsuario.findById(idusuario);
+		if (optional.isPresent()) {
+			return Optional.of(optional.get());
 		}
-		return em.createQuery(criteriaQ).getResultList().get(0);
+		return Optional.empty();
 	}
-	@Override 
-    public Usuario guardar(Usuario usuario) throws Exception {
+
+	@Override
+	public Usuario guardar(Usuario usuario) throws Exception {
 		if(usuario==null){
 			throw new Exception();
 		}
 		return repUsuario.save(usuario);
 	}
-	@Override 
-    public void borrarUsuarioPorRut(String rut) {
-		Usuario usuario = buscarUsuarioPorRut(rut);
-		repUsuario.delete(usuario); 
+
+	@Override
+	public void borrarUsuarioPorRut(String rut) {
+		repUsuario.deleteById(rut);
 	}
 }
