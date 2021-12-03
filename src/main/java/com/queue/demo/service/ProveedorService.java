a4c365b6d109584821cac2cd7fc2b3c1ba350e1d
Catherine Neira <catherine.neira1801@alumnos.ubiobio.cl>
2021-12-03 16:17:52 -0300

    Implementación Testing ProveedorService

--- src/main/java/com/queue/demo/service/ProveedorService.java
+++ src/main/java/com/queue/demo/service/ProveedorService.java
@@ -1,12 +1,13 @@
 package com.queue.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.queue.demo.model.Proveedor;
 
 public interface ProveedorService {
-	public Proveedor buscarPorNombre (String nombre);
-	public Proveedor buscarPorRut (String rut);
+	public Optional<Proveedor> buscarPorNombre (String nombre);
+	public Optional<Proveedor> buscarPorRut (String rut);
 	public List <Proveedor> buscarTodosLosProveedores();
 	boolean eliminarPorNombre(String nombre);
 	boolean eliminarPorRut(String rut);
---
+++ src/test/java/com/queue/demo/service/ProveedorServiceTest.java
@@ -0,0 +1,351 @@
+package com.queue.demo.service;
+
+import com.queue.demo.model.Proveedor;
+import com.queue.demo.model.RepresentanteProveedor;
+import com.queue.demo.repository.RepositorioProveedor;
+import com.queue.demo.repository.RepositorioRepresentanteProveedor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProveedorServiceTest {
+    @Mock
+    private RepositorioProveedor repositorioProveedor;
+
+    @Mock
+    private RepositorioRepresentanteProveedor repositorioRepresentanteProveedor;
+
+    @Mock
+    private EntityManager entityManager;
+
+    @Mock
+    private CriteriaBuilder criteriaBuilder;
+
+    @Mock
+    private CriteriaQuery criteriaQuery=mock(CriteriaQuery.class);
+
+    @Mock
+    private Root<Proveedor> root;
+
+    @Mock
+    private TypedQuery typedQuery=mock(TypedQuery.class);
+
+    @InjectMocks
+    private ProveedorServiceImpl proveedorServiceImpl;
+
+    @Test
+    void SiInvocoBuscarPorNombreYExisteUnoDebeRetornarProveedor() {
+        //Arrange
+        Optional<Proveedor> respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+        mocklist.add(getProveedor());
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("nombreempresa"), getProveedor().getNombreempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        //Act
+        respuesta=proveedorServiceImpl.buscarPorNombre(getProveedor().getNombreempresa());
+
+        //Assert
+        assertNotNull(respuesta);
+        assertEquals(respuesta.get().getRutempresa(),getProveedor().getRutempresa());
+
+    }
+
+    @Test
+    void SiInvocoBuscarPorNombreYNoExisteUnoDebeRetornarNull() {
+        //Arrange
+        Optional<Proveedor> respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("nombreempresa"), getProveedor().getNombreempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        //Act
+        respuesta=proveedorServiceImpl.buscarPorNombre(getProveedor().getNombreempresa());
+
+        //Assert
+        assertNotNull(respuesta);
+        assertTrue(respuesta.isEmpty());
+
+    }
+
+    @Test
+    void SiInvocoBuscarPorRutYExisteUnoDebeRetornarProveedor() {
+        //Arrange
+        Optional<Proveedor> respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+        mocklist.add(getProveedor());
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("rutempresa"), getProveedor().getRutempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        //Act
+        respuesta=proveedorServiceImpl.buscarPorRut(getProveedor().getRutempresa());
+
+        //Assert
+        assertNotNull(respuesta);
+        assertEquals(respuesta.get().getRutempresa(),getProveedor().getRutempresa());
+
+    }
+
+    @Test
+    void SiInvocoBuscarPorRutYNoExisteUnoDebeRetornarNull() {
+        //Arrange
+        Optional<Proveedor> respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("rutempresa"), getProveedor().getRutempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        //Act
+        respuesta=proveedorServiceImpl.buscarPorRut(getProveedor().getRutempresa());
+
+        //Assert
+        assertNotNull(respuesta);
+        assertTrue(respuesta.isEmpty());
+
+    }
+
+    @Test
+    void SiInvocoBuscarTodosLosProveedoresYExistenProveedoresEntoncesRetornaLista(){
+        List<Proveedor> respuesta;
+
+        when(repositorioProveedor.findAll()).thenReturn(getProveedores());
+
+        respuesta=proveedorServiceImpl.buscarTodosLosProveedores();
+        assertNotNull(respuesta);
+        assertEquals(respuesta.size(),getProveedores().size());
+    }
+
+    @Test
+    void SiInvocoBuscarTodosLosProveedoresYNoExistenProveedoresRetornarListaVacia(){
+        List<Proveedor> respuesta;
+        List<Proveedor> mock = new ArrayList<>();
+
+        when(repositorioProveedor.findAll()).thenReturn(mock);
+
+        respuesta=proveedorServiceImpl.buscarTodosLosProveedores();
+        assertNotNull(respuesta);
+        assertEquals(respuesta.size(),0);
+    }
+
+    @Test
+    void SiInvocoGuardarProveedorYNoExisteElProveedorLoInsertaYRetornaProveedor() throws Exception {
+        Proveedor respuesta;
+
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("rutempresa"), getProveedor().getRutempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(new ArrayList<Proveedor>());
+
+        when(repositorioProveedor.save(any())).thenReturn(getProveedor());
+
+        //Act
+        respuesta=proveedorServiceImpl.guardarProveedor(getProveedor());
+
+        //Assert
+        assertEquals(respuesta.getRutempresa(),getProveedor().getRutempresa());
+    }
+
+    @Test
+    void SiInvocoGuardarProveedorYYaExisteElProveedorLanzaUnException() throws Exception{
+
+        List<Proveedor> proveedores = new ArrayList<>();
+        proveedores.add(getProveedor());
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("rutempresa"), getProveedor().getRutempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(proveedores);
+
+        Exception exception = assertThrows(Exception.class,
+                () -> {proveedorServiceImpl.guardarProveedor(getProveedor());});
+
+        assertEquals("Ya existente",exception.getMessage());
+
+
+    }
+
+    @Test
+    void SiInvocoEliminarPorNombreYExisteRetornaTrue(){
+        Boolean respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+        mocklist.add(getProveedor());
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("nombreempresa"), getProveedor().getNombreempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        respuesta= proveedorServiceImpl.eliminarPorNombre(getProveedor().getNombreempresa());
+
+        assertTrue(respuesta);
+    }
+
+    @Test
+    void SiInvocoEliminarPorNombreYNoExisteElProveedorRetornaFalse(){
+        Boolean respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("nombreempresa"), getProveedor().getNombreempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        respuesta=proveedorServiceImpl.eliminarPorNombre(getProveedor().getNombreempresa());
+
+        assertFalse(respuesta);
+
+    }
+
+    @Test
+    void SiInvocoEliminarPorRutYExisteRetornaTrue(){
+        Boolean respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+        mocklist.add(getProveedor());
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("rutempresa"), getProveedor().getRutempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        respuesta= proveedorServiceImpl.eliminarPorRut(getProveedor().getRutempresa());
+
+        assertEquals(respuesta,true);
+    }
+
+    @Test
+    void SiInvocoEliminarPorRutYNoExisteElProveedorRetornaFalse(){
+        Boolean respuesta;
+
+        List<Proveedor> mocklist = new ArrayList<>();
+
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+
+        when(criteriaBuilder.createQuery(any())).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Proveedor.class)).thenReturn(root);
+        when(criteriaQuery.select(root)).thenReturn(criteriaQuery);
+        when(criteriaQuery.where(criteriaBuilder.equal(root.get("rutempresa"), getProveedor().getRutempresa()))).thenReturn(criteriaQuery);
+
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(typedQuery.getResultList()).thenReturn(mocklist);
+
+        respuesta=proveedorServiceImpl.eliminarPorRut(getProveedor().getRutempresa());
+
+        assertEquals(respuesta,false);
+
+    }
+
+
+    private List<Proveedor> getProveedores(){
+        List<Proveedor> proveedores= new ArrayList<>();
+
+        Proveedor prov1 = new Proveedor();
+        prov1.setNombreempresa("Prov1");
+        prov1.setRutempresa("1");
+        prov1.setEstado(true);
+
+        RepresentanteProveedor rep1 = new RepresentanteProveedor();
+        rep1.setEstado(true);
+        rep1.setNombre("A");
+        rep1.setApellido1("B");
+        rep1.setApellido2("C");
+        rep1.setRutrep("1");
+        rep1.setRutemp(prov1.getRutempresa());
+
+
+        prov1.setRepresentanteProveedor(rep1);
+        proveedores.add(prov1);
+
+        Proveedor prov2 = new Proveedor();
+        prov2.setNombreempresa("Prov2");
+        prov2.setRutempresa("2");
+        prov2.setEstado(true);
+        prov2.setRepresentanteProveedor(null);
+        proveedores.add(prov2);
+
+        return proveedores;
+    }
+
+    private Proveedor getProveedor(){
+        return getProveedores().get(0);
+    }
+}
